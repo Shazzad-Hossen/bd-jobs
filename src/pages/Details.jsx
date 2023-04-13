09c70550e3b724c8fcdfe90f07125ab0589133c3
Shazzad Hossen <tipsfire.com@gmail.com>
2023-04-14 03:33:03 +0600

    all error related data loading solved

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -9,73 +9,84 @@ import { addToLocalStorage } from '../utilities/localStorage';
 
 const Details = () => {
     const data= useLoaderData();
-    
-    const {id, logo, des, res, edu, exp, salary, title, contact, location ,company,type1,type2, }= data;
+    console.log(data)
+   
+    const {id, logo, des, res, edu, exp, salary, title, contact, location ,company,type1,type2, }= data ;
     
     const applyjobHandler=()=>{
         addToLocalStorage({id, logo,title,company, type1,type2,location,salary});
         
     }
-   
+
+    
+    
     return (
-        <div className='max-w-[1280px] mx-auto px-6 py-32 flex flex-col md:flex-row   items-center justify-between gap-8'>
-            <div className="max-w-[800px]">
 
-               <p className='text-lg text-[#666666] text-justify pb-6'> <span className='font-bold text-[#000000]'>Job Description:</span> {des} </p>
+        <div className="">
+            {
+                Object.keys(data).length === 0? <div className="text-center text-4xl font-bold p-40">Opps! No jobs Found.</div> :  <div className='max-w-[1280px] mx-auto px-6 py-32 flex flex-col md:flex-row   items-center justify-between gap-8'>
+          
 
-               <p className='text-lg text-[#666666] text-justify pb-6'> <span className='font-bold text-[#000000]'>Job Responsibility:</span> {res} </p>
-
-               <p className='text-lg text-[#666666] text-justify pb-6'> <span className='font-bold text-[#000000]'>Educational Requirements:</span> <br /> <br /> {edu} </p>
-
-               <p className='text-lg text-[#666666] text-justify pb-6'> <span className='font-bold text-[#000000]'>Experiences:</span> <br /> <br />{exp} </p>
-
-
-
-
-            </div>
-            <div className=" w-full max-w-[400px] flex flex-col flex-shrink-0">
-                <div className="bg-gradient-to-r from-[#7E90FE]  to-[#9873FF] rounded-lg">
-            <div className="max-w-[400px] bg-opacity-70 bg-[white]   p-6">
-                <h1 className=' border-b-2 text-xl font-bold text-start py-6'>Job Details</h1>
-
-                <div className="flex items-center justify-start gap-4 py-4 flex-wrap">
-                    <img src={salaryLogo} alt="" />
-                    <p className='text-lg font-semibold'>Salary : </p>
-                    <p className='text-[#7e7e7e]'>{salary} (Per-Month) </p>
+                <div className="max-w-[800px]">
+                            
+                <p className='text-lg text-[#666666] text-justify pb-6'> <span className='font-bold text-[#000000]'>Job Description:</span> {des} </p>
+                
+                <p className='text-lg text-[#666666] text-justify pb-6'> <span className='font-bold text-[#000000]'>Job Responsibility:</span> {res} </p>
+                
+                <p className='text-lg text-[#666666] text-justify pb-6'> <span className='font-bold text-[#000000]'>Educational Requirements:</span> <br /> <br /> {edu} </p>
+                
+                <p className='text-lg text-[#666666] text-justify pb-6'> <span className='font-bold text-[#000000]'>Experiences:</span> <br /> <br />{exp} </p>
+                
+                
+                
+                
                 </div>
-
-                <div className="flex items-center justify-start gap-4 py-4 flex-wrap">
-                    <img src={titleLogo} alt="" />
-                    <p className='text-lg font-semibold'>Job title : </p>
-                    <p className='text-[#7e7e7e]'>{title}  </p>
+                <div className=" w-full max-w-[400px] flex flex-col flex-shrink-0">
+                 <div className="bg-gradient-to-r from-[#7E90FE]  to-[#9873FF] rounded-lg">
+                <div className="max-w-[400px] bg-opacity-70 bg-[white]   p-6">
+                 <h1 className=' border-b-2 text-xl font-bold text-start py-6'>Job Details</h1>
+                
+                 <div className="flex items-center justify-start gap-4 py-4 flex-wrap">
+                     <img src={salaryLogo} alt="" />
+                     <p className='text-lg font-semibold'>Salary : </p>
+                     <p className='text-[#7e7e7e]'>{salary} (Per-Month) </p>
+                 </div>
+                
+                 <div className="flex items-center justify-start gap-4 py-4 flex-wrap">
+                     <img src={titleLogo} alt="" />
+                     <p className='text-lg font-semibold'>Job title : </p>
+                     <p className='text-[#7e7e7e]'>{title}  </p>
+                 </div>
+                
+                 <h1 className=' border-b-2 text-xl font-bold py-6 text-start'>Contact Information</h1>
+                
+                 <div className="flex items-center justify-start gap-4 py-4 flex-wrap">
+                     <img src={phoneLogo} alt="" />
+                     <p className='text-lg font-semibold'>Phone : </p>
+                     <p className='text-[#7e7e7e]'>{contact[1].phone}  </p>
+                 </div>
+                
+                 <div className="flex items-center justify-start gap-4 py-4 flex-wrap">
+                     <img src={emailLogo} alt="" />
+                     <p className='text-lg font-semibold'>E-mail : </p>
+                     <p className='text-[#7e7e7e]'>{contact[0].email}  </p>
+                 </div>
+                
+                 <div className="flex items-center justify-start gap-4 py-4 flex-wrap">
+                     <img src={locationLogo} alt="" />
+                     <p className='text-lg font-semibold'>Address : </p>
+                     <p className='text-[#7e7e7e]'>{location}  </p>
+                 </div>
+                
                 </div>
-
-                <h1 className=' border-b-2 text-xl font-bold py-6 text-start'>Contact Information</h1>
-
-                <div className="flex items-center justify-start gap-4 py-4 flex-wrap">
-                    <img src={phoneLogo} alt="" />
-                    <p className='text-lg font-semibold'>Phone : </p>
-                    <p className='text-[#7e7e7e]'>{contact[1].phone}  </p>
                 </div>
-
-                <div className="flex items-center justify-start gap-4 py-4 flex-wrap">
-                    <img src={emailLogo} alt="" />
-                    <p className='text-lg font-semibold'>E-mail : </p>
-                    <p className='text-[#7e7e7e]'>{contact[0].email}  </p>
+                <button onClick={applyjobHandler} className='primary-btn w-full my-4'>Apply Now</button>
                 </div>
-
-                <div className="flex items-center justify-start gap-4 py-4 flex-wrap">
-                    <img src={locationLogo} alt="" />
-                    <p className='text-lg font-semibold'>Address : </p>
-                    <p className='text-[#7e7e7e]'>{location}  </p>
-                </div>
-
-            </div>
-            </div>
-            <button onClick={applyjobHandler} className='primary-btn w-full my-4'>Apply Now</button>
-            </div>
-            
+                
+                        </div>
+            }
         </div>
+       
     );
 };
 
--- src/utilities/dataLoader.js
+++ src/utilities/dataLoader.js
@@ -1,20 +1,19 @@
-export const dataLoader=async()=>{
+export const dataLoader = async () => {
+  const data = await fetchData("/jobs.json");
+  return data;
+};
+export const jobLoader = async (jID) => {
+  const data = await fetchData("/jobs.json");
+  const job = data.find((j) => j.id === Number(jID));
+  if(job===undefined)  return {};
+  return job;
  
+};
 
-    const data= await fetchData('jobs.json');
-   
-    return  data;
-    
-}
-export const jobLoader=async(jID)=>{
-    const data= await fetchData('/jobs.json');
-    const job=data.find(j=>j.id===Number(jID))
-    return  job;
-    
-}
-
-export const fetchData=async(url)=>{
-    const res= await fetch(url);
-    const data= await res.json();
-    return data;
-}
+export const fetchData = async (url) => {
+  const res = await fetch(url);
+  const data = await res.json();
+  
+ return data;
+  
+};
